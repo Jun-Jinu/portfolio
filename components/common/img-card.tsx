49a38c77e2d5fcf187e5afac469901cee7f5415b
ForwardOoh <soa9719@naver.com>
2023-04-20 00:29:42 +0900

    feat: 이미지 카드 컴포넌트

---
+++ components/common/img-card.tsx
@@ -0,0 +1,111 @@
+import styled from "styled-components";
+
+interface CardProps {
+    category: string[];
+    title: string;
+    tag: string[];
+    image: string;
+}
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    justify-content: center;
+    width: 100%;
+    max-width: 300px;
+    height: auto;
+    background-color: #fff;
+    border-radius: 12px;
+    box-shadow: 0 3px 8px rgba(0, 0, 0, 0.12);
+    overflow: hidden;
+    transition: all 0.2s ease-in-out;
+
+    &:hover {
+        transform: translateY(-5px);
+        box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);
+    }
+`;
+
+const TextContainer = styled.div`
+    padding: 25px;
+`;
+
+const Image = styled.img`
+    min-width: 100%;
+    height: 240px;
+    object-fit: cover;
+    object-position: center;
+    border-top-left-radius: 12px;
+    border-top-right-radius: 12px;
+`;
+
+const CategoryContainer = styled.h4`
+    display: flex;
+    font-size: 18px;
+    font-weight: 400;
+    color: #000;
+    margin: 0 0 10px;
+`;
+
+const Title = styled.h3`
+    font-size: 24px;
+    font-weight: 700;
+    color: #000;
+    margin: 20px 0 10px;
+`;
+
+const TagContainer = styled.p`
+    display: flex;
+`;
+
+const TagText = styled.p`
+    font-size: 16px;
+    font-weight: 400;
+    color: #909499;
+    margin: 0 8px 20px 0;
+`;
+
+const Bubble = styled.div`
+    background-color: #f2f2f2;
+    border-radius: 4px;
+    padding: 5px 10px;
+    max-width: 200px;
+    margin-right: 10px;
+    display: flex;
+    justify-content: center;
+`;
+
+const BubbleText = styled.p`
+    color: #333;
+    font-size: 16px;
+    margin: 0;
+`;
+
+const ImgCard = ({ category, title, tag, image }: CardProps) => {
+    return (
+        <Container>
+            <TextContainer>
+                <CategoryContainer>
+                    {category.map((el, index) => (
+                        <Bubble key={index}>
+                            <BubbleText>{el}</BubbleText>
+                        </Bubble>
+                    ))}
+                </CategoryContainer>
+
+                <Title>{title}</Title>
+
+                <TagContainer>
+                    {tag.map((el, index) => (
+                        <TagText key={index}>#{el}</TagText>
+                    ))}
+                </TagContainer>
+            </TextContainer>
+
+            <Image src={image} alt={title} />
+        </Container>
+    );
+};
+
+export default ImgCard;
---
+++ components/common/skill.tsx
@@ -0,0 +1,72 @@
+import styled from "styled-components";
+
+interface Skill {
+    name: string;
+    percentage: number;
+}
+
+interface SkillListProps {}
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+const Title = styled.h2`
+    color: #333;
+    font-size: 24px;
+    margin-bottom: 20px;
+`;
+
+const SkillContainer = styled.div`
+    display: flex;
+    align-items: center;
+    margin-bottom: 20px;
+`;
+
+const Name = styled.p`
+    color: #333;
+    font-size: 20px;
+    margin-right: 10px;
+`;
+
+const Bar = styled.div<{ percentage: number }>`
+    width: 300px;
+    height: 10px;
+    background-color: #f2f2f2;
+    border-radius: 10px;
+    position: relative;
+
+    &::before {
+        content: "";
+        display: block;
+        width: ${(props) => props.percentage}%;
+        height: 100%;
+        background-color: #3498db;
+        border-radius: 10px;
+        position: absolute;
+        top: 0;
+        left: 0;
+    }
+`;
+
+export default function SkillList() {
+    const skills: Skill[] = [
+        { name: "Skill 1", percentage: 70 },
+        { name: "Skill 2", percentage: 90 },
+        { name: "Skill 3", percentage: 80 },
+        { name: "Skill 4", percentage: 60 },
+    ];
+
+    return (
+        <Container>
+            <Title>Skills</Title>
+            {skills.map((skill, index) => (
+                <SkillContainer key={index}>
+                    <Name>{skill.name}</Name>
+                    <Bar percentage={skill.percentage} />
+                </SkillContainer>
+            ))}
+        </Container>
+    );
+}
