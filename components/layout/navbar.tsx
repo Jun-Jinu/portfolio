fe05ca6b0154db9277bec6c6f6f93a10a0b4e561
ForwardOoh <soa9719@naver.com>
2023-04-19 20:39:12 +0900

    feat: 상단바 다크모드, 언어 추가

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -44,7 +44,7 @@ const NavMenuLink = styled.a<{ isDarkMode: boolean }>`
   margin: 0 1rem;
   cursor: pointer;
   transition: color 0.2s ease-in-out;
-  font-size: 16px;
+  font-size: 20px;
   padding: 28px 0;
 
   &:hover {
@@ -56,8 +56,8 @@ const NavMainLink = styled.a<{ isDarkMode: boolean }>`
   margin: 0 1rem;
   cursor: pointer;
   transition: color 0.2s ease-in-out;
-  font-size: 16px;이거
-  padding: 28px 0;이거
+  font-size: 28px;
+  padding: 24px 0;
 
   &:hover {
     color: ${({ isDarkMode }) => (isDarkMode ? '#ccc' : '#666')};
@@ -84,7 +84,8 @@ const Navbar: React.FC<NavbarProps> = ({ darkMode, toggleDarkMode, lang, toggleL
       <NavMenuContainer>
         <NavMenuLink isDarkMode={darkMode} href="/skills">
           Skills
-        </NavMenuLink>
+        </NavMenuLink>\
+        
         <NavMenuLink isDarkMode={darkMode} href="/career">
           Career
         </NavMenuLink>
@@ -94,10 +95,24 @@ const Navbar: React.FC<NavbarProps> = ({ darkMode, toggleDarkMode, lang, toggleL
       </NavMenuContainer>
       <NavRightContainer>
         <NavButton onClick={toggleDarkMode}>
-          {darkMode ? 'Light mode' : 'Dark mode'}
+        { darkMode ? (
+          // 달 이미지
+          <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
+            <path data-v-9f42629e="" d="M16.085 13.417c-5.013 0-9.076-4.04-9.076-9.023 0-1.596.42-3.093 1.152-4.394C3.58.456 0 4.3 0 8.977 0 13.961 4.064 18 9.076 18c3.407 0 6.372-1.868 7.924-4.628-.3.03-.605.045-.914.045z" transform="translate(-1344 -24) translate(1248 24) translate(96) translate(4 3)"></path>
+          </svg>
+        ) : (
+          // 해 이미지
+        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
+          <path d="M11.888 6.22c1.586 0 3.023.643 4.063 1.682 1.04 1.04 1.682 2.476 1.682 4.063 0 1.587-.643 3.023-1.682 4.063-1.04 1.04-2.477 1.682-4.063 1.682-1.587 0-3.023-.643-4.063-1.682-1.04-1.04-1.682-2.476-1.682-4.063 0-1.587.643-3.023 1.682-4.063 1.04-1.04 2.476-1.683 4.063-1.683z" transform="translate(-1423 -168) translate(79.5 145) translate(1248 24) translate(96)"></path>
+          <path stroke-width="1.5" d="M18.972 4.88l-1.154 1.154 1.154-1.154 1.154-1.154-1.154 1.154zM4.608 19.245L3.453 20.4l1.155-1.155 1.154-1.154-1.154 1.154zM21.948 12.063L20.315 12.063 21.948 12.063 23.58 12.063zM1.632 12.063L0 12.063 1.632 12.063 3.265 12.063zM18.973 19.245l-1.154-1.154 1.154 1.154 1.154 1.155-1.154-1.155zM4.608 4.88L3.453 3.726 4.608 4.88l1.154 1.154L4.608 4.88zM11.824 1.905L11.824 3.538 11.824 1.905 11.824.273zM11.824 22.22L11.824 23.853 11.824 22.22 11.824 20.588z" transform="translate(-1423 -168) translate(79.5 145) translate(1248 24) translate(96)"></path>
+        </svg>
+        )}
         </NavButton>
+        
+
+
         <NavButton onClick={toggleLang}>
-          {lang === 'en' ? '한국어' : 'English'}
+          {lang === 'eng' ? 'ENG' : 'KOR'}
         </NavButton>
       </NavRightContainer>
     </NavbarContainer>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,24 @@
+import { useState } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
 import Navbar from '@/components/layout/navbar'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [darkMode, setDarkMode] = useState(false);
+  const [lang, setLang] = useState("kor");
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
+  const toggleLang = () => {
+    setLang(lang === 'kor' ? 'eng' : 'kor');
+  };
+
   return (
     <>
       <Head>
@@ -15,106 +27,12 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
+      <main>
 
-        <Navbar darkMode={false} toggleDarkMode={function (): void {
-          throw new Error('Function not implemented.')
-        } } lang={''} toggleLang={function (): void {
-          throw new Error('Function not implemented.')
-        } }></Navbar>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
+        <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} lang={lang} toggleLang={toggleLang} />
 
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-        </div>
 
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
+        
       </main>
     </>
   )
