83802c1cf5c5304cdce9f079e56b76045ee68278
ForwardOoh <soa9719@naver.com>
2023-05-09 03:05:10 +0900

    feat: 리액트 토스트 추가

--- pages/project/[project-name].tsx
+++ pages/project/[project-name].tsx
@@ -1,15 +1,54 @@
 import { NextPage } from "next";
 import { useRouter } from "next/router";
+import styled from "styled-components";
+import { useEffect } from "react";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const Container = styled.div`
+    padding: 100px;
+`;
+
+const Title = styled.h2`
+    margin: 0 0 50px;
+`;
+
+const Description = styled.p`
+    margin-bottom: 100px;
+`;
 
 const Project: NextPage = () => {
     const router = useRouter();
-    console.log(router);
-    const projectName = router.query["project-name"]; // Here, 'project' is the name of the dynamic parameter in the URL
+    const projectName = router.query["project-name"];
+
+    useEffect(() => {
+        // Show a toast notification when the component mounts
+        projectName ? toast.success(`Welcome to ${projectName}!`) : null;
+    }, [projectName]);
+
     return (
-        <div>
-            <h1>Project: {projectName}</h1>
-            <p>Welcome to {projectName}!</p>
-        </div>
+        <Container>
+            <Title>Project: {projectName}</Title>
+            <Description>
+                This is a description of the project, which could include
+                information about the technology used, your role in the project,
+                and other relevant details.
+            </Description>
+            <Title>Function 1</Title>
+            <Description>
+                This is a description of the first implemented function, which
+                could include details about its purpose, how it works, and any
+                challenges you faced while implementing it.
+            </Description>
+            <Title>Function 2</Title>
+            <Description>
+                This is a description of the second implemented function, which
+                could include details about its purpose, how it works, and any
+                challenges you faced while implementing it.
+            </Description>
+            {/* Repeat for any other implemented functions */}
+            <ToastContainer />
+        </Container>
     );
 };
 
